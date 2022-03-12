974a6cf3b5e097624e5c183f93515b081ca4d9f0
Blaise1030 <61928648+Blaise1030@users.noreply.github.com>
2022-03-12 16:44:47 +0800

    Removed button from box

--- src/lib/box/Box.svelte
+++ src/lib/box/Box.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import type { IStyleInterface } from '../system/styleProps';
-	import { crossfade } from 'svelte/transition';
 	import generateStylesClass from '../system/styleComposer';
-	import Button from '$lib/Button/Button.svelte';
 	export let sp: Partial<IStyleInterface> = {};
 	export let as: string = 'div';
 
